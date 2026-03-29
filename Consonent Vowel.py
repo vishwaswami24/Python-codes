b9df85b014799249bcd90b46dee88ac1165a73eb
Vishwa Swami <swamivishwa0@gmail.com>
2026-03-29 12:00:00 +0000

    \Add Consonent Vowel.py" --date 2026-03-29T12:00:00+00:00

---
+++ Consonent Vowel.py
@@ -0,0 +1,18 @@
+ch=input('Enter character:')
+
+if ch=='a' or ch=='A':
+    print('Vowel')
+elif ch=='e' or ch=='E':
+    print('Vowel')    
+elif ch=='i' or ch=='I':
+    print('Vowel')    
+elif ch=='o' or ch=='O':
+    print('Vowel')    
+elif ch=='u' or ch=='U':
+    print('Vowel')
+else:
+    print('Consonent')
+ 
+ 
+ 
+ 
