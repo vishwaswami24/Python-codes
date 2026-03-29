89b8482d281a5f7c7cbef1a570f9c8ce3209df42
Vishwa Swami <swamivishwa0@gmail.com>
2026-03-29 12:00:00 +0000

    \Add break statement.py" --date 2026-03-29T12:00:00+00:00

---
+++ break statement.py
@@ -0,0 +1,51 @@
+#Break
+
+i=1
+while i<=10:
+    if i==5:
+        i=i+1
+        continue
+    print(i,end=' ')
+    i=i+1
+print('loop break')
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
