13fa988fcbff308f7a9bca29493840d17c30f70c
Vishwa Swami <swamivishwa0@gmail.com>
2026-03-29 12:00:00 +0000

    \Add Arithmetic operators.py" --date 2026-03-29T12:00:00+00:00

---
+++ Arithmetic operators.py
@@ -0,0 +1,33 @@
+#Values
+x=10
+y=2
+
+#Addition
+z=x+y
+print('Addition =',z)
+
+#Subtraction
+p=y-x
+print('Subtraction =',p)
+
+#Multiplication
+q=x*y
+print('Multiplication =',q)
+
+#Division
+r=x/y
+print('Division =',r)
+
+#Modulus
+t=x%y
+print('Mod =',t)
+
+#floor divison
+s=x//y
+print('floordiv =',s)
+
+#Exponent
+u=x**y               #10*10
+print('Exponent =',u)
+
+
