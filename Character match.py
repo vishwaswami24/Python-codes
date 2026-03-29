e633927f11818595c3e4204e58435992b0d71eeb
Vishwa Swami <swamivishwa0@gmail.com>
2026-03-29 12:00:00 +0000

    \Add Character match.py" --date 2026-03-29T12:00:00+00:00

---
+++ Character match.py
@@ -0,0 +1,10 @@
+ch=input('Enter chatacter:')
+
+if ch=='a':
+    print('Character Matched')
+elif ch=='b':
+    print('Character Matched')
+elif ch=='c':
+    print('Character Matched')
+else:
+    print('Character not Matched')
