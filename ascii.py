f52bfdff61a0b817899566da626bc1c2db8aa391
Vishwa Swami <swamivishwa0@gmail.com>
2026-03-29 12:00:00 +0000

    \Add ascii.py" --date 2026-03-29T12:00:00+00:00

---
+++ ascii.py
@@ -0,0 +1,13 @@
+
+'''
+for i in range(1,5):
+    
+    for j in range(1,5):
+        print(chr(64+i),end='')
+    print()    
+'''
+for i in range(1,5):
+    
+    for j in range(1,5):
+        print(chr(96+j),end='')
+    print() 
