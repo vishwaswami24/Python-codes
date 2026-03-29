e57205bbfc298e7e370a9ee3c73dc3114c300c65
Vishwa Swami <swamivishwa0@gmail.com>
2026-03-29 12:00:00 +0000

    \Add arithmatic.py" --date 2026-03-29T12:00:00+00:00

---
+++ arithmatic.py
@@ -0,0 +1,12 @@
+def add(x,y):
+    z=x+y
+    print('Addition:',z)
+def sub(x,y):
+    z=x-y
+    print('Subtraction:',z)
+def mul(x,y):
+    z=x*y
+    print('Multiplication:',z)
+def div(x,y):
+    z=x//y
+    print('Division:',z)
