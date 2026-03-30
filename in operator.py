0231aa6259247a665d8e232fec23d6b6b850cc26
Vishwa Swami <swamivishwa0@gmail.com>
2026-03-30 12:00:00 +0000

    \Add in operator.py" --date 2026-03-30T12:00:00+00:00

---
+++ in operator.py
@@ -0,0 +1,20 @@
+'''
+ch=input('Enter character:')
+
+w='aeiou'
+
+if ch in w:
+    print('Vowel')
+else:
+    print('Consonent')
+'''
+
+n=int(input('Enter Number:'))
+
+m=[1,2,3,4,5,6,7,8,9,0]
+
+if n in m:
+    print('Valid number')
+else:
+    print('Invalid number')
+    
