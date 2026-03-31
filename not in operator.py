c99edc6ded4690eafbbcbc96cae261bbeee5ebd6
Vishwa Swami <swamivishwa0@gmail.com>
2026-03-31 12:00:00 +0000

    \Add not in operator.py" --date 2026-03-31T12:00:00+00:00

---
+++ not in operator.py
@@ -0,0 +1,17 @@
+'''
+n=int(input('Enter number:'))
+
+if n not in(10,11):
+    print('Valid Number')
+else:
+    print('Invalid Number')
+'''
+
+x='itvedant'
+
+st=input('Enter the string:')
+
+if st not in x:
+    print('Not Found')
+else:
+    print('Found')
