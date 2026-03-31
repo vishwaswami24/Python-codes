5fa0b0821cf940cfe85d69951ff3285eb5cc724a
Vishwa Swami <swamivishwa0@gmail.com>
2026-03-31 12:00:00 +0000

    \Add math module.py" --date 2026-03-31T12:00:00+00:00

---
+++ math module.py
@@ -0,0 +1,28 @@
+'''
+import math
+
+print(math.ceil(40.3876))
+print(math.floor(40.3876))
+
+
+#Module aliasing
+
+import math as m        
+
+print(m.sqrt(225))
+print(m.pi)
+'''
+
+#import specific functions
+
+from math import factorial,sqrt
+
+print('factorial is:',factorial(10))
+print('Square root is:',sqrt(100))
+
+#import all functions
+
+from math import *
+
+print(tau)
+
