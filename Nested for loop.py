730c0ea9dbd89473e06a0623fcc95679042d8562
Vishwa Swami <swamivishwa0@gmail.com>
2026-03-31 12:00:00 +0000

    \Add Nested for loop.py" --date 2026-03-31T12:00:00+00:00

---
+++ Nested for loop.py
@@ -0,0 +1,20 @@
+#Nested for loop
+'''
+for i in range(1,4):
+    for j in range(1,4):
+        print('i=',i,'j=',j)
+'''
+
+for i in range(1,6):
+    
+    for j in range(1,6):
+        print('*',end='')
+        
+    print()
+'''
+for i in range(1,5):
+    
+    for j in range(1,6):
+        print(j,end=' ')
+    print()    
+'''
