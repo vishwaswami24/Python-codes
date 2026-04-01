7a418480e25c731f527bbe07d20b3486cdcc525c
Vishwa Swami <swamivishwa0@gmail.com>
2026-04-01 12:00:00 +0000

    \Add random module.py" --date 2026-04-01T12:00:00+00:00

---
+++ random module.py
@@ -0,0 +1,14 @@
+import random as r    
+'''
+s=r.random()
+print(s)
+
+t=10000*r.random()
+print(t)
+
+res=round(t)
+print(res)
+'''
+print(round(10000*r.random()))
+
+print(r.randrange(1000,9999))
