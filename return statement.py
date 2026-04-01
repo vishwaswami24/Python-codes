ee671c50f9b6e2db937b73be243fc646dedd58e2
Vishwa Swami <swamivishwa0@gmail.com>
2026-04-01 12:00:00 +0000

    \Add return statement.py" --date 2026-04-01T12:00:00+00:00

---
+++ return statement.py
@@ -0,0 +1,17 @@
+'''
+def add(x,y):
+    z=x+y
+    return z
+a=add(10,20)
+print(a)
+'''
+def display(x,y):
+    a=x+y
+    b=x-y
+    c=x*y
+    return a,b,c
+d=display(50,20)
+print(d)
+
+ad,sub,mul=display(50,20)
+print(ad,sub,mul)
