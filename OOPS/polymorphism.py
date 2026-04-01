20f89e18b20af3a2f2fa204cf6a0b53c00d07a81
Vishwa Swami <swamivishwa0@gmail.com>
2026-04-01 12:00:00 +0000

    \Add polymorphism.py" --date 2026-04-01T12:00:00+00:00

---
+++ OOPS/polymorphism.py
@@ -0,0 +1,13 @@
+x=int(input('Enter the 1st value:'))
+y=int(input('Enter the 2nd value:'))
+
+z=x+y
+
+print('Addition is:',z)
+
+a=input('Enter the 1st name:')
+b=input('Enter the 2nd name:')
+
+c=a+' '+b
+
+print('Addition is:',c)
