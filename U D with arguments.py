3065032450e80ea88c6dca78d5e88d81dc1cf641
Vishwa Swami <swamivishwa0@gmail.com>
2026-04-02 12:00:00 +0000

    \Add U D with arguments.py" --date 2026-04-02T12:00:00+00:00

---
+++ U D with arguments.py
@@ -0,0 +1,8 @@
+def multiplication(a,b):
+    print(a)
+    print(b)
+    c=a*b
+    print('Multiplication is:',c)
+x=int(input('Enter 1st number:'))
+y=int(input('Enter 2nd number:'))
+multiplication(x,y)
