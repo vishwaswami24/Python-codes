a8fbbb6d2100dd93d63d8671b0a23be257d1b542
Vishwa Swami <135796111+vishwaswami24@users.noreply.github.com>
2026-04-16 11:20:31 +0530

    Refactor prime number detection algorithm
    Refactor prime number checking logic to improve efficiency and clarity.

--- prime number.py
+++ prime number.py
@@ -1,11 +1,10 @@
-#prime number
-
-n=int(input('Enter number:'))
-
-for i in range(2,n,1):
-    r=n%i
-    if r==0:
-        print('Not Prime')
-        break
+num = 
+if num > 1:
+    for i in range(2, int(num**0.5) + 1):
+        if (num % i) == 0:
+            print(f"{num} is a composite number")
+            break
+    else:
+        print(f"{num} is a prime number")
 else:
-        print('Prime')
+    print(f"{num} is neither prime nor composite")
